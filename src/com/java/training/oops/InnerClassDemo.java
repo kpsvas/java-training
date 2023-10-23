2b9f22f19d0dbe8fec9c027349a58162666e18ce
Srinivas Komatipally <kpsvas@gmail.com>
2023-10-22 20:57:57 -0400

    updated excercise programs

---
+++ src/com/java/training/oops/InnerClassDemo.java
@@ -0,0 +1,20 @@
+package com.java.training.oops;
+
+class OuterClassA {
+    int x = 10;
+
+    class InnerClassB {
+        int y = 5;
+    }
+}
+
+public class InnerClassDemo {
+
+    public static void main(String[] args) {
+        OuterClassA myOuter = new OuterClassA();
+        OuterClassA.InnerClassB myInner = myOuter.new InnerClassB();
+        System.out.println(myInner.y + myOuter.x);
+
+    }
+}
+
